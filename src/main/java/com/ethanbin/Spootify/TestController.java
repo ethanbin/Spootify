dab4710f40846037d0a407bbdc96bc61af5dd49f
Ethan <ethanbinya@aol.com>
2024-03-29 02:11:15 -0400

    Bundle vite-react frontend with backend

---
+++ src/main/java/com/ethanbin/Spootify/TestController.java
@@ -0,0 +1,13 @@
+package com.ethanbin.Spootify;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+//    @GetMapping(value="/")
+    //@RequestMapping(value="/",method=RequestMethod.GET)
+//    public String hello(){
+//        return "Hello World!!";
+//    }
+}
+
